9bd63b0f86503d0d2185b821fb691aac2dbb2bcc
sushso <sophie.victoria@viacesi.fr>
2014-02-05 11:50:01 +0100

    d

--- src/miniSIG/Sig.java
+++ src/miniSIG/Sig.java
@@ -1,10 +1,17 @@
 package miniSIG;
 
-public class Sig {
+import Data.StcMsg;
 
+public class Sig {
+private static Controller.Processus oP = new Controller.Processus();
+private static StcMsg oMsg = new StcMsg();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+            
+            Sig.oMsg.data = new Object[2];
+            Sig.oMsg.data[0] = "test1";
+            Sig.oMsg.data[1] = "test2";
+            Sig.oMsg = oP.addLocation(oMsg);
 	}
 
 }
