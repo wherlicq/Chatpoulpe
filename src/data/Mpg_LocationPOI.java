d23378fbd9d0995ef2ff8a6fe2c5b9ba05e7aa34
sushso <sophie.victoria@viacesi.fr>
2014-02-05 14:53:09 +0100

    change de moi de locationpoi et cad
    By El Sush'

--- src/data/Mpg_LocationPOI.java
+++ src/data/Mpg_LocationPOI.java
@@ -10,6 +10,105 @@ package Data;
  *
  * @author Sush
  */
-public class Mpg_LocationPOI {
+public class Mpg_LocationPOI implements Mpg {
     
+    private StcMsg oMsg;
+
+    @Override
+    public StcMsg getAll(StcMsg oMsg) {
+        
+        this.oMsg = new StcMsg();
+        
+        this.oMsg.rqSql = "SELECT * FROM `locationpoi`";
+        
+        return this.oMsg;
+    }
+
+    @Override
+    public StcMsg getById(StcMsg oMsg) {
+        
+        Integer id;
+        
+        this.oMsg = new StcMsg();
+        
+        id = (Integer)oMsg.data[0];
+        
+        this.oMsg.rqSql = "SELECT * FROM `locationpoi` WHERE `IDLocation` = ?";
+        this.oMsg.data = new Object[1];
+        this.oMsg.data[0] = id;
+       
+        return this.oMsg;
+    }
+    
+    public StcMsg getByIdPoi(StcMsg oMsg) {
+        
+        Integer id;
+        
+        this.oMsg = new StcMsg();
+        
+        id = (Integer)oMsg.data[0];
+        
+        this.oMsg.rqSql = "SELECT * FROM `locationpoi` WHERE `IDPoi` = ?";
+        this.oMsg.data = new Object[1];
+        this.oMsg.data[0] = id;
+       
+        return this.oMsg;
+    }
+
+    @Override
+    public StcMsg add(StcMsg oMsg) {
+        
+        Integer idlocation;
+        Integer idpoi;
+        
+        this.oMsg = new StcMsg();
+        
+        idlocation = (Integer)oMsg.data[0];
+        idpoi = (Integer)oMsg.data[1];  
+
+        this.oMsg.rqSql = "INSERT INTO `locationpoi`(`IDLocation`, `IDPoi`) VALUES (?,?)";
+        this.oMsg.data = new Object[2];
+        this.oMsg.data[0] = idlocation;
+        this.oMsg.data[1] = idpoi;
+
+        return this.oMsg;
+    }
+
+    @Override
+    public StcMsg update(StcMsg oMsg) {
+        
+        Integer idlocationnew;
+        Integer idpoi;
+        Integer idlocation;
+        
+        this.oMsg = new StcMsg();
+        
+        idlocationnew = (Integer)oMsg.data[0];
+        idlocation = (Integer)oMsg.data[1];
+        idpoi = (Integer)oMsg.data[2]; 
+
+        this.oMsg.rqSql = "UPDATE `locationpoi` SET `IDLocation`= ?` WHERE `IDPoi` = ? AND `IDLocation` = ?";
+        this.oMsg.data = new Object[3];
+        this.oMsg.data[0] = idlocationnew;
+        this.oMsg.data[1] = idpoi;
+        this.oMsg.data[2] = idlocation;
+        
+        return this.oMsg;
+    }
+
+    @Override
+    public StcMsg deleteById(StcMsg oMsg) {
+ 
+        Integer id;
+        
+        this.oMsg = new StcMsg();
+        
+        id = (Integer)oMsg.data[0];
+        
+        this.oMsg.rqSql = "DELETE FROM `locationpoi` WHERE `IDPoi` = ?";
+        this.oMsg.data = new Object[1];
+        this.oMsg.data[0] = id;
+        
+        return this.oMsg;
+    }
 }
--- src/data/Mpg_LocationTrip.java
+++ src/data/Mpg_LocationTrip.java
@@ -12,4 +12,5 @@ package Data;
  */
 public class Mpg_LocationTrip {
     
+    
 }
