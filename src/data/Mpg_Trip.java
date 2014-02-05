4acb6eff3e07151ab4b3b28ce8e0a1d7ce7ad831
sushso <sophie.victoria@viacesi.fr>
2014-02-05 11:06:51 +0100

    change de moi de trip
    By El Sush'

--- src/data/Mpg_Trip.java
+++ src/data/Mpg_Trip.java
@@ -10,6 +10,92 @@ package minisig.Data;
  *
  * @author Sush
  */
-public class Mpg_Trip {
+public class Mpg_Trip implements Mpg {
+
+    private StcMsg oMsg;
+    
+    @Override
+    public StcMsg getAll(StcMsg oMsg) {
+        
+        this.oMsg = new StcMsg();
+        
+        this.oMsg.rqSql = "SELECT * FROM `trip`";
+        
+        return this.oMsg;
+    }
+
+    @Override
+    public StcMsg getById(StcMsg oMsg) {
+        
+        Integer id;
+        
+        this.oMsg = new StcMsg();
+        
+        id = (Integer)oMsg.data[0];
+        
+        this.oMsg.rqSql = "SELECT * FROM `trip` WHERE `IDTrip` = ?";
+        this.oMsg.data = new Object[1];
+        this.oMsg.data[0] = id;
+       
+        return this.oMsg;
+    }
+
+    @Override
+    public StcMsg add(StcMsg oMsg) {
+        
+        String label;
+        String text;
+        
+        this.oMsg = new StcMsg();
+        
+        label = (String)oMsg.data[0];
+        text = (String)oMsg.data[1];
+        
+        
+        this.oMsg.rqSql = "INSERT INTO `trip`(`LabelTrip`, `TextTrip`) VALUES (?,?)";
+        this.oMsg.data = new Object[2];
+        this.oMsg.data[0] = label;
+        this.oMsg.data[1] = text;
+
+        return this.oMsg;
+    }
+
+    @Override
+    public StcMsg update(StcMsg oMsg) {
+        
+        Integer id;
+        String label;
+        String text;
+        
+        this.oMsg = new StcMsg();
+        
+        id = (Integer)oMsg.data[0];
+        label = (String)oMsg.data[1];
+        text = (String)oMsg.data[2];  
+
+        this.oMsg.rqSql = "UPDATE `trip` SET `LabelTrip`= ?,`TextTrip`= ? WHERE `IDTrip` = ?";
+        this.oMsg.data = new Object[3];
+        this.oMsg.data[0] = id;
+        this.oMsg.data[1] = label;
+        this.oMsg.data[2] = text;
+        
+        return this.oMsg;
+    }
+
+    @Override
+    public StcMsg deleteById(StcMsg oMsg) {
+        
+        Integer id;
+        
+        this.oMsg = new StcMsg();
+        
+        id = (Integer)oMsg.data[0];
+        
+        this.oMsg.rqSql = "DELETE FROM `trip` WHERE `IDTrip` = ?";
+        this.oMsg.data = new Object[1];
+        this.oMsg.data[0] = id;
+        
+        return this.oMsg;
+    }
     
 }
