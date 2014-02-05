84fc7c912f99d28eca8df2f83763a324e77ace8e
sushso <sophie.victoria@viacesi.fr>
2014-02-05 14:23:44 +0100

    change de moi de link et image
    By El Sush'

--- src/data/Mpg_LinkHyperText.java
+++ src/data/Mpg_LinkHyperText.java
@@ -10,6 +10,79 @@ package Data;
  *
  * @author Sush
  */
-public class Mpg_LinkHyperText {
+public class Mpg_LinkHyperText implements Mpg {
     
+    private StcMsg oMsg;
+
+    @Override
+    public StcMsg getAll(StcMsg oMsg) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StcMsg getById(StcMsg oMsg) {
+        
+        Integer id;
+        
+        this.oMsg = new StcMsg();
+        
+        id = (Integer)oMsg.data[0];
+        
+        this.oMsg.rqSql = "SELECT * FROM `linkhypertext` WHERE `IDLinkHyperText` = ?";
+        this.oMsg.data = new Object[1];
+        this.oMsg.data[0] = id;
+       
+        return this.oMsg;
+    }
+
+    @Override
+    public StcMsg add(StcMsg oMsg) {
+        
+        String link;
+        
+        this.oMsg = new StcMsg();
+        
+        link = (String)oMsg.data[0];
+
+        this.oMsg.rqSql = "INSERT INTO `linkhypertext`(`LinkHyperText`) VALUES (?)";
+        this.oMsg.data = new Object[1];
+        this.oMsg.data[0] = link;
+
+        return this.oMsg;
+    }
+
+    @Override
+    public StcMsg update(StcMsg oMsg) {
+        
+        Integer id;
+        String link;
+        
+        this.oMsg = new StcMsg();
+        
+        link = (String)oMsg.data[0];
+        id = (Integer)oMsg.data[1];
+
+        this.oMsg.rqSql = "UPDATE `linkhypertext` SET `LinkHyperText`= ? WHERE `IDLinkHyperText` = ?";
+        this.oMsg.data = new Object[2];
+        this.oMsg.data[0] = link;
+        this.oMsg.data[1] = id;
+        
+        return this.oMsg;
+    }
+
+    @Override
+    public StcMsg deleteById(StcMsg oMsg) {
+        
+        Integer id;
+        
+        this.oMsg = new StcMsg();
+        
+        id = (Integer)oMsg.data[0];
+        
+        this.oMsg.rqSql = "DELETE FROM `linkhypertext` WHERE `IDLinkHyperText` = ?";
+        this.oMsg.data = new Object[1];
+        this.oMsg.data[0] = id;
+        
+        return this.oMsg;
+    }
 }
