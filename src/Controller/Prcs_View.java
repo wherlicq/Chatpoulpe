eb078e2357f0b99083657b3c6df6c3590e787a61
sushso <sophie.victoria@viacesi.fr>
2014-02-07 09:46:50 +0100

    qzefg

--- src/Controller/Prcs_View.java
+++ src/Controller/Prcs_View.java
@@ -10,13 +10,23 @@ public class Prcs_View {
 	private Processus oP;
 	private Home_Window home;
 	private Main_Window main;
+        private StcMsg oMsg;
 	
 	public void initialize() throws ClassNotFoundException, SQLException
 	{
-		StcMsg oMsg = new StcMsg();
-		oMsg.data = new Object[1];
-		oMsg.data[0] = 1;
-		oP = new Processus();
-		oP.loadLocation(oMsg);
+                home = new Home_Window(this);
+                home.setVisible(true);
 	}
+        
+        public StcMsg initializeNews() throws SQLException, ClassNotFoundException
+        {
+            StcMsg oMsga = new StcMsg();
+            this.oMsg = new StcMsg();
+            oMsga.data = new Object[1];
+            oMsga.data[0] = true;
+            this.oP = new Processus();
+            this.oMsg = this.oP.loadNews(oMsga);
+            
+            return this.oMsg;
+        }
 }
--- src/GUI/Home_Window.java
+++ src/GUI/Home_Window.java
@@ -5,9 +5,12 @@ package GUI;
 *@author Poulp
 */
 
+import Controller.Prcs_View;
+import Data.StcMsg;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -20,32 +23,49 @@ import javax.swing.SwingConstants;
 
 
 public class Home_Window extends JFrame{
-	public Home_Window(){
+    
+    Prcs_View oPv;
+    StcMsg oMsg;
+    
+	public Home_Window(final Prcs_View oPv) throws SQLException, ClassNotFoundException{
 		
+               this.oPv = oPv;
 		JPanel main_panel = new JPanel();
 		JPanel lower_panel = new JPanel();
 		JPanel upper_panel = new JPanel();
 		JPanel button_panel = new JPanel();
 		JLabel news = new JLabel("Chatpoulpe");
 		final JTextArea textarea = new JTextArea();
+                
+                this.oMsg = this.oPv.initializeNews();
+                for(int i = 0; i < this.oMsg.selectedData.length; i++)
+                {
+                    textarea.append("Nouveauté " + (i+1) + ":\n");
+                    textarea.append((String)this.oMsg.selectedData[i][1] + "\n");
+                    textarea.append((String)this.oMsg.selectedData[i][2] + "\n");
+                    textarea.append("\n");
+                }
+                
+                
 		JButton display_button = new JButton("Carte");
 		display_button.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				System.out.println("Clic Carte");
+				Main_Window mW = new Main_Window(oPv);
+                                mW.setVisible(true);
 				
 			}
 		});
-		JButton help_button = new JButton("Aide");
-		help_button.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				Help_Window help = new Help_Window();
-				
-			}
-		});
+//		JButton help_button = new JButton("Aide");
+//		help_button.addActionListener(new ActionListener() {
+//			
+//			@Override
+//			public void actionPerformed(ActionEvent arg0) {
+//				Help_Window help = new Help_Window();
+//				
+//			}
+//		});
 		JButton exit_button = new JButton("Quitter");
 		exit_button.addActionListener(new ActionListener() {
 			
@@ -66,10 +86,11 @@ public class Home_Window extends JFrame{
 		textarea.setWrapStyleWord(true);
 		textarea.setColumns(30);
 		textarea.setRows(15);
+                textarea.setEditable(false);
 		display_button.setMinimumSize(new Dimension(300, 300));
 		display_button.setMaximumSize(new Dimension(300, 300));
-		help_button.setMinimumSize(new Dimension(200, 200));
-		help_button.setMaximumSize(new Dimension(300, 300));
+//		help_button.setMinimumSize(new Dimension(200, 200));
+//		help_button.setMaximumSize(new Dimension(300, 300));
 		exit_button.setMinimumSize(new Dimension(200, 200));
 		exit_button.setMaximumSize(new Dimension(300, 300));
 		
@@ -87,7 +108,7 @@ public class Home_Window extends JFrame{
 		button_panel.setLayout(new BoxLayout(button_panel, BoxLayout.Y_AXIS));
 		button_panel.add(display_button);
 		button_panel.add(new Box.Filler(minSize, prefSize, maxSize));
-		button_panel.add(help_button);
+//		button_panel.add(help_button);
 		button_panel.add(new Box.Filler(minSize, prefSize, maxSize));
 		button_panel.add(exit_button);
 		this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.X_AXIS));
@@ -97,5 +118,7 @@ public class Home_Window extends JFrame{
 		
 		this.getContentPane().add(main_panel);
 		display_button.setHorizontalTextPosition(SwingConstants.LEFT);
+                
+                
 	}
 }
